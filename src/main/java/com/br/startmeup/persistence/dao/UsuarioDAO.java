551279d63cadae1689eb1c054f469a32f0432136
bruno <brunoalbuquerquebrito@outlook.com>
2019-01-21 13:45:58 -0200

    Removento classes desnecessarias

--- src/main/java/com/br/startmeup/persistence/dao/UsuarioDAO.java
+++ src/main/java/com/br/startmeup/persistence/dao/UsuarioDAO.java
@@ -44,13 +44,15 @@ public class UsuarioDAO implements IUsuarioDAO<Usuario> {
             try {
                 connection.getOutputStream().write(urlParamters.getBytes());
                 int responseCode = connection.getResponseCode();
-                if (responseCode == HttpURLConnection.HTTP_OK)
+                if (responseCode == HttpURLConnection.HTTP_OK){
                     connection.disconnect();
-                return true;
+                    return true;
+                }
+                connection.disconnect();
             } catch (Exception e) {
-
+                connection.disconnect();
             }
-            connection.connect();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -91,59 +93,16 @@ public class UsuarioDAO implements IUsuarioDAO<Usuario> {
 
     @Override
     public List<Usuario> findAll() {
-
-        List<Usuario> usuarios = new ArrayList<>();
-
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM usuario");
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                Usuario usuario = new Usuario();
-                usuario.setId(rs.getLong("id"));
-                usuario.setNome(rs.getString("nome"));
-                usuario.setEmail(rs.getString("email"));
-                usuario.setSenha(rs.getString("senha"));
-                usuarios.add(usuario);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return usuarios;
+        return null;
     }
 
     @Override
     public boolean update(Usuario usuario) {
-        String sql = "UPDATE usuario SET nome=?, email=?, senha=? WHERE id = ?";
-
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setString(1, usuario.getNome());
-            ps.setString(2, usuario.getEmail());
-            ps.setString(3, usuario.getSenha());
-            ps.setLong(4, usuario.getId());
-
-            if (ps.executeUpdate() != 0) {
-                return true;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
         return false;
     }
 
     @Override
     public boolean delete(Usuario usuario) {
-        String sql = "DELETE FROM usuario WHERE id = ?";
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ps.setLong(1, usuario.getId());
-            if (ps.executeUpdate() != 0) {
-                return true;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
         return false;
     }
 
@@ -173,8 +132,8 @@ public class UsuarioDAO implements IUsuarioDAO<Usuario> {
                         usuario = new Gson().fromJson(inputLine,Usuario.class);
                     }
                     in.close();
-                    connection.disconnect();
                 }
+                connection.disconnect();
             } catch (Exception e) {
                 e.printStackTrace();
             }
